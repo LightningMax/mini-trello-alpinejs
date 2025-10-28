cf04842fae4bbe40b56aabdcdd9ed15d5ff609b3
Max Reis <maxreis507@gmail.com>
2025-10-28 11:44:57 +0100

    feat: add modify modal component

--- src/card.js
+++ src/card.js
@@ -4,17 +4,34 @@ export default () => ({
   status: "to do",
   cards: [],
   search: "",
+  editIndex: null,
 
   addCard() {
     this.cards.push({
+      index: this.cards.length - 1,
       title: this.title,
       description: this.description,
       status: this.status,
     });
+    this.title = "";
+    this.description = "";
+    this.status = "to do";
   },
 
-  modifyCard(index) {
-    this.cards.splice(index, 1);
+  modifyCard() {
+    if (this.editIndex != null) {
+      this.cards[this.editIndex].title = this.title;
+      this.cards[this.editIndex].description = this.description;
+      this.cards[this.editIndex].status = this.status;
+      this.reset();
+    }
+  },
+
+  reset() {
+    this.title = "";
+    this.description = "";
+    this.status = "to do";
+    this.editIndex = null;
   },
 
   get filteredCards() {
--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ document.addEventListener("alpine:init", () => {
 
   Alpine.store("filteredCards", { status: "all" });
 
+  Alpine.store("modifyModal", {
+    on: false,
+
+    toggle() {
+      this.on = !this.on;
+    },
+
+    open(index) {
+      console.log(index);
+      const card = Alpine.store("card");
+      card.editIndex = index;
+      console.log(card.editIndex);
+      card.title = card.cards[index].title;
+      card.description = card.cards[index].description;
+      card.status = card.cards[index].status;
+      this.on = true;
+    },
+
+    close() {
+      const card = Alpine.store("card");
+      card.reset();
+      this.on = false;
+    },
+  });
+
   Alpine.store("cardModal", {
     on: false,
 
